501de2e8df971785f118cbb157290af637b5026e
ThaiHuuNguyen <106139705+ThaiHuuNguyen@users.noreply.github.com>
2022-07-06 09:27:13 +0700

    Update content_script.js

--- chrome extension/content_script.js
+++ chrome extension/content_script.js
@@ -1,31 +1,4 @@
-//  const listHoatChat = [];
-//x = 0;
-// chrome.runtime.onMessage.addListener(function(msg, sender, sendresponse) {
 
-
-//     const hoatchatTest = document.getElementById("hoatchat").value;
-//     const test1 = listHoatChat.find(item => item === hoatchatTest);
-//     console.log(test1);
-//     if (test1) {
-//         var result = 'inputs: ' + test1;
-//         result += '<br /> output: ' + 'dữ liệu' + ' ' + test1 + ' ' + 'đã tồn tại vui lòng nhập lại';
-//     } else {
-//         listHoatChat[x] = hoatchatTest;
-//         const inputFields = document.getElementById("hoatchat").value;
-//         var result = 'inputs: ' + inputFields;
-//         const test = document.getElementById('hoatchat1').innerHTML;
-//         result += '<br /> output: ' + test;
-//     }
-
-//     sendresponse(result);
-
-
-// });
-
-
-// var html = document.createElement('div');
-// html.innerHTML = '<input type="button" id="appendButton" name="button" class="button button1" value="Thêm" onclick="insertNewRecord()">';
-// document.body.appendChild(html);
 const listHoatChat = [];
 x = 0;
 document.getElementById('appendButton').addEventListener('click', function(Response) {
@@ -41,8 +14,8 @@ document.getElementById('appendButton').addEventListener('click', function(Respo
             listHoatChat[x] = hoatchatTest;
             chrome.runtime.onMessage.addListener(function(msg, sender, sendresponse) {
                 const inputFields = document.getElementById("hoatchat").value;
-                var result = 'inputs: ' + inputFields;
-                const test = document.getElementById('hoatchat1').innerHTML;
+                var result = 'hoatchat: ' + inputFields;
+                const test = document.getElementById('hienthi').innerHTML;
                 result += '<br /> output: ' + test;
                 sendresponse(result);
             });
